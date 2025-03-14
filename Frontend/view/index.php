984c18a589673888cade55fceea431bf01c3d590
Thomas <152426390+CThomassss@users.noreply.github.com>
2025-03-14 13:23:56 +0100

    Update Titre

--- Frontend/view/index.php
+++ Frontend/view/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Concessionnaire Isuzu & Subaru</title>
+    <title>Concessionnaire MondialAutomobile</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
